596d681d52d8c17ad270b97ae8348177a5c5553b
tong <tong@disktree.net>
2023-05-07 03:49:29 +0200

    Move loading theme from css into main app script

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,17 +1,34 @@
 'use strict';
 
-function updatescrolltopbuttom() {
+console.info('%chttps://github.com/verein-fuer-elektro-akustik/velak.klingt.org', 'background:#000;color:#fff;padding:1rem;');
+
+let theme;
+
+function updateScrollTopButtom() {
     const btn = document.body.querySelector('i.ic-arrow_upward');
     if(btn) {
         btn.style.display = (document.body.scrollHeight > window.innerHeight) ? "block":"none";
     }
 }
-window.addEventListener("load", e => {
-    console.info("%c⬤","color:#fff;background:#000;padding:3rem 3rem;font-size:32px;");
-    //console.info('%cDEVELOPER: https://disktree.net', 'background:#000;color:#fff;padding:20px;');
-    //console.info('%cSOURCE: https://github.com/verein-fuer-elektro-akustik/velak.klingt.org', 'background:#000;color:#fff;padding:1rem;');
-    updatescrolltopbuttom();
-    window.addEventListener("resize", e => {
-        updatescrolltopbuttom();
+window.addEventListener("load", _ => {
+
+    const style = window.getComputedStyle(document.querySelector(':root'));
+    theme = {
+        background: style.getPropertyValue("--background"),
+        f_high: style.getPropertyValue("--f_high"),
+        f_med: style.getPropertyValue("--f_med"),
+        f_low: style.getPropertyValue("--f_low"),
+        f_inv: style.getPropertyValue("--f_inv"),
+        b_high: style.getPropertyValue("--b_high"),
+        b_med: style.getPropertyValue("--b_med"),
+        b_low: style.getPropertyValue("--b_low"),
+        b_inv: style.getPropertyValue("--b_inv"),
+    };
+
+    updateScrollTopButtom();
+
+    window.addEventListener("resize", _ => {
+        updateScrollTopButtom();
     }, false);
+
 }, false);
