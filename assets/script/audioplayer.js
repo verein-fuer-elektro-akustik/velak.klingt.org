760281884cb75f80ac5092f3a3e9a10edc437d52
tong <tong@disktree.net>
2023-05-07 03:48:25 +0200

    Wrap audioplayer into class

--- assets/script/audioplayer.js
+++ assets/script/audioplayer.js
@@ -1,153 +1,121 @@
 "use strict";
 
-let theme;
-let container, statusElement, canvas, graphics;
-let url;
-let audio, gain, analyser, freqData, timeData;
-let animationFrameId;
-
-function onAnimationFrame(time) {
-
-    animationFrameId = window.requestAnimationFrame(onAnimationFrame);
-
-    analyser.getByteTimeDomainData(timeData);
-    analyser.getByteFrequencyData(freqData);
-    //analyser.getFloatTimeDomainData(floatData);
-
-    graphics.fillStyle = theme.background;
-    graphics.fillRect(0, 0, canvas.width, canvas.height);
-    graphics.fillStyle = theme.f_med;
-    graphics.lineWidth = 1;
-
-    drawTimedata(0, 0, canvas.width, canvas.height);
-    //drawFrequencies(0, 0, canvas.width, canvas.height);
-
-    updateStatusText(time);
-}
-
-function drawTimedata(x, y, width, height) {
-    graphics.beginPath();
-    graphics.strokeStyle = theme.f_med;
-    const sw = width * 1.0 / analyser.frequencyBinCount;
-    const cy = height / 2;
-    let px = 0.0, py = 0.0;
-    for (let i = 0; i < analyser.frequencyBinCount; i++) {
-        py = (timeData[i] / 128.0) * cy;
-        if (i == 0) {
-            graphics.moveTo(x + px, y + py);
-        } else {
-            graphics.lineTo(x + px, y + py);
+class Audioplayer {
+    constructor(container) {
+        this.container = container;
+        this.statusElement = container.querySelector('.status');
+        this.canvas = container.querySelector('canvas.spectrum');
+        const r = container.getBoundingClientRect();
+        this.canvas.height = r.height - r.y;
+        this.canvas.width = r.width - r.x;
+        this.graphics = this.canvas.getContext("2d");
+    }
+    play = (url) => {
+        if(this.audio) {
+            //this.stop();
         }
-        px += sw;
-    }
-    graphics.stroke();
-}
-
-function drawFrequencies(x, y, width, height) {
-    graphics.fillStyle = theme.f_med;
-    const res = analyser.frequencyBinCount;
-    let bwidth = Math.floor(width / res);
-    if (bwidth <= 0) bwidth = 1;
-    let bheight;
-    let px = 0;
-    for (let i = 0; i < res; i++) {
-        bheight = freqData[i] * 2; // / 2;
-        graphics.fillRect(x + px, height - bheight - y, bwidth, 1);
-        px += bwidth;
-    }
-}
-
-function updateStatusText(time, extraText) {
-    let str = "";
-    if (time) str += time;
-    str += "<br>" + url;
-    if (extraText) str += " " + extraText;
-    statusElement.innerHTML = str;
-}
-
-function play(url) {
-    audio = document.createElement('audio');
-    audio.preload = "none";
-    audio.crossOrigin = "anonymous";
-    audio.controls = false;
-    audio.onplaying = _ => {
-        const audioContext = new AudioContext();
-        gain = audioContext.createGain();
-        //gain.gain.value = 0.9;
-        gain.connect(audioContext.destination);
-        analyser = audioContext.createAnalyser();
-        analyser.fftSize = 1024;
-        //analyser.smoothingTimeConstant = 0.8;
-        //analyser.minDecibels = -140;
-        //analyser.maxDecibels = 0;
-        analyser.connect(gain);
-        freqData = new Uint8Array(analyser.frequencyBinCount);
-        timeData = new Uint8Array(analyser.frequencyBinCount);
-        //floatData = new Float32Array(analyser.fftSize);
-        const media = audioContext.createMediaElementSource(audio);
-        media.connect(analyser);
-        animationFrameId = window.requestAnimationFrame(onAnimationFrame);
-    }
-    const sourceElement = document.createElement('source');
-    sourceElement.src = url;
-    audio.append(sourceElement);
-    audio.play();
-}
-
-function stopStream() {
-    if (audio) {
-        audio.pause();
-        audio.onpause = _ => {
-            audio = null;
+        this.audio = document.createElement('audio');
+        this.audio.preload = "none";
+        this.audio.crossOrigin = "anonymous";
+        this.audio.controls = false;
+        this.audio.onplaying = _ => {
+            const audioContext = new AudioContext();
+            this.gain = audioContext.createGain();
+            //gain.gain.value = 0.9;
+            this.gain.connect(audioContext.destination);
+            this.analyser = audioContext.createAnalyser();
+            this.analyser.fftSize = 1024;
+            //analyser.smoothingTimeConstant = 0.8;
+            //analyser.minDecibels = -140;
+            //analyser.maxDecibels = 0;
+            this.analyser.connect(this.gain);
+            this.freqData = new Uint8Array(this.analyser.frequencyBinCount);
+            this.timeData = new Uint8Array(this.analyser.frequencyBinCount);
+            this.floatData = new Float32Array(this.analyser.fftSize);
+            const media = audioContext.createMediaElementSource(this.audio);
+            media.connect(this.analyser);
+            this.animationFrameId = window.requestAnimationFrame(this.onAnimationFrame);
         }
-        graphics.clearRect(0, 0, canvas.width, canvas.height);
-        if (animationFrameId) {
-            window.cancelAnimationFrame(animationFrameId);
-            animationFrameId = null;
+        const sourceElement = document.createElement('source');
+        sourceElement.src = url;
+        this.audio.append(sourceElement);
+        this.audio.play();
+    }
+    onAnimationFrame = (time) => {
+        this.animationFrameId = window.requestAnimationFrame(this.onAnimationFrame);
+        this.analyser.getByteTimeDomainData(this.timeData);
+        this.analyser.getByteFrequencyData(this.freqData);
+        this.analyser.getFloatTimeDomainData(this.floatData);
+        this.graphics.fillStyle = theme.background;
+        this.graphics.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.graphics.fillStyle = theme.f_med;
+        this.graphics.lineWidth = 1;
+        this.drawTimedata(0, 0, this.canvas.width, this.canvas.height);
+        //drawFrequencies(24,  canvas.height-48, canvas.width-48, canvas.height/2);
+        //drawFrequencies(0,  0, canvas.width, canvas.height);
+        this.updateStatusText(time);
+    }
+    drawTimedata = (x, y, width, height) => {
+        this.graphics.beginPath();
+        this.graphics.strokeStyle = theme.f_med;
+        const sw = width * 1.0 / this.analyser.frequencyBinCount;
+        const cy = height / 2;
+        let px = 0.0, py = 0.0;
+        for (let i = 0; i < this.analyser.frequencyBinCount; i++) {
+            py = (this.timeData[i] / 128.0) * cy;
+            if (i == 0) {
+                this.graphics.moveTo(x + px, y + py);
+            } else {
+                this.graphics.lineTo(x + px, y + py);
+            }
+            px += sw;
+        }
+        this.graphics.stroke();
+    }
+    /*
+    drawFrequencies(x, y, width, height) => {
+        //graphics.fillStyle = '#ff0000';
+        //graphics.fillRect(x, y, width, height);
+        graphics.fillStyle = theme.f_med;
+        const barWidth = Math.floor(width / analyser.frequencyBinCount);
+        let barHeight;
+        let px = 0;
+        for (let i = 0; i < analyser.frequencyBinCount; i++) {
+            barHeight = freqData[i];
+            if(barHeight < 1) {
+                barHeight = 0;
+            } else {
+                graphics.fillRect(x + px, y + height/2 + barHeight, barWidth, 1);
+            }
+            px += barWidth;
         }
     }
-}
-
-function fitCanvas() {
-    if (canvas) {
-        const container = canvas.parentElement;
+    */
+    updateStatusText = (time, extraText) => {
+        let str = "";
+        if (time) str += time;
+        str += "<br>" + url;
+        if (extraText) str += " " + extraText;
+        this.statusElement.innerHTML = str;
+    }
+    fitCanvas = () => {
+        const container = this.canvas.parentElement;
         const r = container.getBoundingClientRect();
         const W = Math.round(r.width - r.x);
         const H = Math.round(r.height - r.y);
-        canvas.width = W;
-        canvas.height = H;
+        this.canvas.width = W;
+        this.canvas.height = H;
     }
 }
 
-window.addEventListener('load', _ => {
-
-    container = document.body.querySelector('div.audioplayer');
-    if (!container)
-        return;
-
-    url = container.getAttribute('data-url');
-
-    statusElement = container.querySelector('.status');
-    canvas = container.querySelector('canvas.spectrum');
-    graphics = canvas.getContext("2d");
-
-    const style = window.getComputedStyle(document.querySelector(':root'));
-    theme = {
-        background: style.getPropertyValue("--background"),
-        f_high: style.getPropertyValue("--f_high"),
-        f_med: style.getPropertyValue("--f_med"),
-        f_low: style.getPropertyValue("--f_low"),
-        f_inv: style.getPropertyValue("--f_inv"),
-        b_high: style.getPropertyValue("--b_high"),
-        b_med: style.getPropertyValue("--b_med"),
-        b_low: style.getPropertyValue("--b_low"),
-        b_inv: style.getPropertyValue("--b_inv"),
-    };
-
-    fitCanvas();
-    window.onresize = _ => { fitCanvas(); }
-
-    container.onclick = _ => {
-        play(url);
-    };
+let container = document.body.querySelector('div.audioplayer');
+let url = container.getAttribute('data-url');
+var audioplayer = new Audioplayer(container);
+audioplayer.fitCanvas()
+container.addEventListener('click', _ => {
+    audioplayer.play(url);
 }, false);
+window.addEventListener("resize", _ => {
+    //audioplayer.fitCanvas();
+}, false);
+
